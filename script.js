fd4ac1f0a95da2cb6096caacc138c343adb36759
Easybaba1067 <149300599+Easybaba1067@users.noreply.github.com>
2025-06-04 22:33:51 +0100

    update version

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const mode = document.querySelector(".theme");
 const body = document.querySelector("body");
 const nav = document.querySelectorAll(".navigator");
+const testimonys = document.querySelectorAll(".testimony");
+const clickH1 = document.querySelector(".testimonies h1");
 
 mode.addEventListener("click", () => {
   mode.classList.toggle("active");
@@ -13,3 +15,23 @@ nav.forEach((navigator) => {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   });
 });
+
+myClick = 0;
+maxSlide = testimonys.length;
+
+function goToSlide(myClick) {
+  testimonys.forEach((testimony, i) => {
+    testimony.style.transform = `translateX(${150 * (i - myClick)}%)`;
+  });
+}
+goToSlide(myClick);
+function slideShow(params) {
+  if (myClick === 0) {
+    myClick = maxSlide - 1;
+  } else {
+    myClick--;
+  }
+  goToSlide(myClick);
+}
+clickH1.addEventListener("click", slideShow);
+setInterval(slideShow, 3000);
